642c1c5e14b3fbf179f39cda215ae77e81d09b8b
Your Name <you@example.com>
2019-09-24 21:22:31 +0000

    peer programmed with nmehanma

--- timer1.js
+++ timer1.js
@@ -3,13 +3,16 @@ const args = process.argv.slice([2]);
 
 const timer = function(args) {
   let alarmTimes = args;
-  
   alarmTimes.sort(function(a, b)  {
     return a - b;
   });
+
   for (let i = 0; i < alarmTimes.length; i++) {
+    if (alarmTimes[i] >= 1)
   setTimeout(()=> {process.stdout.write('\x07')}, alarmTimes[i] * 1000);
-  }
-  return args;
+  } 
+  
 };
 timer(args);
+
+
